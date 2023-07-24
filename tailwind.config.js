239129c64c0e302254072f10593f978ee6a5a354
Mohammad Emran <phpfour@gmail.com>
2023-07-24 21:59:32 +0600

    Add Tailwind CSS and Livewire 3 beta

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp')
+  ],
+}
